098a9ae7c502befd5352daa54d1fe6d846acbdc6
Srinivas Gorur-Shandilya <code@srinivas.gs>
2023-10-17 09:55:01 -0400

    clean up

--- python/keep_jupyter_running.sh
+++ python/keep_jupyter_running.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+
 # get list of things running 
-servers=$(jupyter lab list)
+servers=$(/opt/homebrew/bin/jupyter lab list)
 echo $servers
 
 
 if [[ $servers == *"token"* ]]; then
   # Jupyter lab is running, do nothing
-  echo "Jupyter running, nothing to do" >> ~/Desktop/log.txt
+  echo "Jupyter running, nothing to do" >> ~/Library/logs/jupyter.log
 else
   # not running, start it
-  echo "Jupyter NOT running, attempting to start..." >> ~/Desktop/log.txt
+  echo "Jupyter NOT running, attempting to start..." >> ~/Library/logs/jupyter.log
   /opt/homebrew/bin/jupyter lab --ip $HOSTNAME --notebook-dir ~/code/ --no-browser 
   echo "DONE"
 fi
